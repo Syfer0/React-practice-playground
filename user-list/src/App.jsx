2eaf9f1d11186cf0a41575f72d5702bc73bef71e
Arman <125900320+Syfer0@users.noreply.github.com>
2024-01-09 17:58:46 +0530

    hooks

---
+++ user-list/src/App.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [users, setUsers] = useState[""];
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then(setUsers);
+  });
+  return null;
+}
+
+export default App;
